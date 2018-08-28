513c66651f056388510a14091a88f0f328353ada
Diogo Dutra <diogodutra@gmail.com>
2018-08-28 09:07:58 -0300

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,5 @@
 import numpy as np
 from physics_sim import PhysicsSim
-from math import log10
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
@@ -19,7 +18,6 @@ class Task():
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
         self.action_repeat = 3
 
-        #self.state_size = self.action_repeat * 6
         state = self.get_state()
         self.state_size = self.action_repeat * state.shape[0]
         self.action_low = 0
@@ -35,31 +33,25 @@ class Task():
         
         reward -= 0.3*(abs(self.sim.pose[:3] - self.target_pos)).sum() #punishes wrong position
         reward -= 0.1*(abs(self.sim.v)).sum() #punishes non-static state
-        reward -= 1.1*(abs(self.sim.angular_v)).sum() #punishes rotational state
-        
-        #reward -= 1.3*abs(self.sim.pose[2] - self.target_pos[2]) #punishes wrong position
-        #reward -= 1.1*abs(self.sim.v[2]) #punishes non-static state
-        #reward -= 1.1*abs(self.sim.angular_v[1]) #punishes rotating state
+        reward -= 1.1*(abs(self.sim.angular_v)).sum() #punishes rotating state
         
         return reward
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
         reward = 0
-        pose_all = []
+        state_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
             reward += self.get_reward() / self.action_repeat
-            #pose_all.append(self.sim.pose)
             state = self.get_state()
-            pose_all.append(state)
-        next_state = np.concatenate(pose_all)
+            state_all.append(state)
+        next_state = np.concatenate(state_all)
         return next_state, reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        #state = np.concatenate([self.sim.pose] * self.action_repeat) 
         state = self.get_state()
         state = np.concatenate([state] * self.action_repeat) 
         return state
